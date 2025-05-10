4fa03269ccac563659560bb292ed892c0ada3313
Gabriel Furtado <Gabriel.dsousa@sptech.school>
2025-05-10 01:25:00 -0300

    Front do questionario feito

--- Site - API/web-data-viz/src/controllers/formularioController.js
+++ Site - API/web-data-viz/src/controllers/formularioController.js
@@ -3,25 +3,25 @@ var formularioModel = require("../models/formularioModel");
 function enviar(req, res) {
     // Recuperando os valores do corpo da requisição
     let classeFormulario = req.body.classeFormularioServer;
-    let titutlo = req.body.titutloServer;
+    let titulo = req.body.tituloServer;
     let mensagem = req.body.mensagemServer;
-    let id = req.body.idServer;
+    let fkUsuario = req.body.idServer;
 
-    console.log("Dados recebidos:", {classeFormulario, titutlo, mensagem, id});
+    console.log("Dados recebidos:", {classeFormulario, titulo, mensagem, fkUsuario});
 
     // Validações dos valores
     if (classeFormulario == undefined) {
         res.status(400).send("Seu select está undefined!");
-    } else if (titutlo == undefined) {
+    } else if (titulo == undefined) {
         res.status(400).send("Seu titulo está undefined!");
     } else if (mensagem == undefined) {
         res.status(400).send("Sua mensagem está undefined!");
-    } else if (id == undefined) {
+    } else if (fkUsuario == undefined) {
         res.status(400).send("Sua id está undefined!");
     }
     else {
         // Passando os valores para o model
-        formularioModel.enviar(classeFormulario, titutlo, mensagem, id)
+        formularioModel.enviar(classeFormulario, titulo, mensagem, fkUsuario)
             .then(
                 function (resultado) {
                     res.json(resultado);
